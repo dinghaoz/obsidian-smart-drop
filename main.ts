d81527f7b09b56bbf1ca3ab252ddbfcfcafbb460
Dinghao Zeng <dinghao.zeng@gmail.com>
2023-10-28 22:10:12 +0800

    test data transfer content

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  Editor,
+  htmlToMarkdown,
+  MarkdownFileInfo,
+  MarkdownView,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting
+} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -10,73 +20,100 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
+export default class SmartDropPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.app.workspace.on(
+			"editor-paste",
+			(evt: ClipboardEvent, editor: Editor, info: MarkdownView | MarkdownFileInfo) => {
+				console.log("editor-past")
+				if (evt.clipboardData === null) { return }
+				if (!evt.defaultPrevented) {
+					evt.preventDefault()
+				}
+				this.onEditorDataTransfer(evt.clipboardData, editor, info)
+			}
+		)
+
+		this.app.workspace.on(
+			"editor-drop",
+			(evt: DragEvent, editor: Editor, info: MarkdownView | MarkdownFileInfo) => {
+				console.log("editor-drop")
+				if (evt.dataTransfer === null) { return }
+              if (!evt.defaultPrevented) {
+                evt.preventDefault()
+              }
+				this.onEditorDataTransfer(evt.dataTransfer, editor, info)
+			}
+		)
 	}
+
+
+	private async onEditorDataTransfer(dataTransfer: DataTransfer, editor: Editor, _: MarkdownView | MarkdownFileInfo) {
+
+
+		const uriList = dataTransfer.getData("text/uri-list")
+		const html = dataTransfer.getData("text/html")
+		const plain = dataTransfer.getData("text/plain")
+
+      	console.log("uri-list", uriList)
+      	console.log("html", html)
+      	console.log("plain", plain)
+
+	  	if (html.length == 0) { return }
+		const markdown = htmlToMarkdown(html)
+
+      	console.log("markdown", markdown)
+
+	  	editor.replaceSelection(markdown)
+
+
+		try {
+			// const activeFile = this.getCurrentNote()
+			// const fItems = evt.clipboardData.files
+			// const tItems = evt.clipboardData.items
+			//
+			// for (const key in tItems) {
+			//
+			// 	// Check if it was a text/html
+			// 	if (tItems[key].kind == "string") {
+			//
+			// 		if (this.settings.realTimeUpdate) {
+			// 			const cont = htmlToMarkdown(evt.clipboardData.getData("text/html")) +
+			// 				htmlToMarkdown(evt.clipboardData.getData("text"))
+			// 			for (const reg_p of MD_SEARCH_PATTERN) {
+			// 				if (reg_p.test(cont)) {
+			//
+			// 					showBalloon("Media links were found, processing...", this.settings.showNotifications)
+			//
+			// 					this.enqueueActivePage(activeFile)
+			// 					this.setupQueueInterval()
+			// 					break
+			// 				}
+			// 			}
+			// 		}
+			// 		return
+			// 	}
+			//
+			// }
+		} catch (e) {
+			new Notice(`Error ${e}`)
+			return
+		}
+	}
+
 
 	onunload() {
 
@@ -91,26 +128,26 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+// class SampleModal extends Modal {
+// 	constructor(app: App) {
+// 		super(app);
+// 	}
+//
+// 	onOpen() {
+// 		const {contentEl} = this;
+// 		contentEl.setText('Woah!');
+// 	}
+//
+// 	onClose() {
+// 		const {contentEl} = this;
+// 		contentEl.empty();
+// 	}
+// }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: SmartDropPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SmartDropPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,7 +158,7 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
+			.setName('Smart Drop Setting #1')
 			.setDesc('It\'s a secret')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
